0c4e93022668b0d244dfe88853db0846ffbfa4a1
Travis <tdelkreyes@gmail.com>
2023-07-17 09:45:45 -0400

    put place player and circle before place_player()

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -3,16 +3,17 @@ from gasp import *
 begin_graphics()            
 finished = False
 
+def place_player():
+    print("Here I am!")
+    Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+
+def move_player():
+    print("I'm moving...")
+    update_when('key_pressed')
+
 place_player()
 
 while not finished:
     move_player()
 
 end_graphics()  
-
-def place_player():
-    print("Here I am!")
-
-def move_player():
-    print("I'm moving...")
-    update_when('key_pressed')
