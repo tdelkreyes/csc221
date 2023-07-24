6f6e3006da538edb76329fd190854207515cee33
Travis <tdelkreyes@gmail.com>
2023-07-24 10:52:23 -0400

    added player_shape and made fixes

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -1,18 +1,25 @@
 from gasp import * 
 from random import randint
+begin_graphics()
 
-player_x = randint(0, 63)
-player_y = randint(0, 47)
+def place_robot():
+    global T1000,navx,navy
+    navx = randint(0, 63)
+    navy = randint(0, 47)
+    T1000 = Circle((10 * navx + 5, 10 * navy + 5), 5, color="red", filled=True)
+    print("Destroy all Humans")
 
 def place_player():
-    global baller
-
-    baller = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
-    print("Here I am!")
+    global player_shape,player_x,player_y
+    player_x = randint(0, 63)
+    player_y = randint(0, 47)
+    player_shape = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
 
 def move_player(): 
-    global player_x, player_y, baller
+    global player_x, player_y, player_shape
+
     key = update_when('key_pressed')
+
     if key == '6' and player_x < 63:
         player_x += 1
     elif key == '3':
@@ -35,24 +42,33 @@ def move_player():
             player_x -= 1
         if player_y < 47:
             player_y += 1
-
+    
     elif key == '8' and player_y < 47:
         player_y += 1
-
     elif key == '9':
         if player_x < 63:
             player_x += 1
         if player_y < 47:
             player_y += 1
-    move_to(baller, (10 * player_x + 5, 10 * player_y + 5))
 
-begin_graphics()
+    move_to(player_shape, (10 * player_x + 5, 10 * player_y + 5))
 
+def move_robot():
+    global navx, navy, T1000, player_x, player_y
+    if player_x > navx:
+        navx += 1
+    elif player_x < navx:
+        navx -= 1
+    elif player_y > navy:
+        navy += 1
+    elif player_y < navy:
+        navy -= 1
+    move_to(T1000, (10 * navx + 5, 10 * navy + 5))
+
+place_robot()
 place_player()
-
-finished = False 
-
+finished = False
 while not finished:
     move_player()
-
-end_graphics()     
+    move_robot()
+end_graphics()    
