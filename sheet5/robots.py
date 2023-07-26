89bc2a69b741a8d7c1768586354f52ad6e2ed474
Travis <tdelkreyes@gmail.com>
2023-07-26 10:30:19 -0400

    changed navx and navy to robot.x and robot.y

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -1,18 +1,28 @@
 from gasp import * 
 from random import randint
 begin_graphics()
+
+class Player:
+    pass
+
+class Robot:
+    pass
+
 def place_robot():
-    global T1000,navx,navy
-    navx = randint(0, 63)
-    navy = randint(0, 47)
-    T1000 = Circle((10 * navx + 5, 10 * navy + 5), 5, color="red", filled=True)
+    global robot
+    robot = Robot
+    robot.x = randint(0, 63)
+    robot.y = randint(0, 47)
+    robot.form = Circle((10 * robot.x + 5, 10 * robot.y + 5), 5, color="red", filled=True)
+
 def safe_player_place():
-    global player_shape,player_x,player_y, navx,navy
+    global player
+    player = Player()
     safety = 0
     player_x = randint(0, 63)
     player_y = randint(0, 47)
     while safety == 0:
-        if navx == player_x and navy == player_y:
+        if robot.x == player_x and robot.y == player_y:
             player_x = randint(0, 63)
             player_y = randint(0, 47)
         else:
@@ -20,7 +30,7 @@ def safe_player_place():
             safety += 1
 
 def move_player(): 
-    global player_x, player_y, player_shape
+    global player
 
     key = update_when('key_pressed')
 
@@ -61,32 +71,38 @@ def move_player():
         if player_y < 47:
             player_y += 1
     move_to(player_shape, (10 * player_x + 5, 10 * player_y + 5))
+
 def move_robot():
-    global navx, navy, T1000, player_x, player_y
-    if player_x > navx:
-        navx += 1
-    elif player_x < navx:
-        navx -= 1
-    elif player_y > navy:
-        navy += 1
-    elif player_y < navy:
-        navy -= 1
-    move_to(T1000, (10 * navx + 5, 10 * navy + 5))
+    global player
+    if player_x > robot.x:
+        robot.x += 1
+    elif player_x < robot.x:
+        robot.x -= 1
+    elif player_y > robot.y:
+        robot.y += 1
+    elif player_y < robot.y:
+        robot.y -= 1
+    move_to(robot, (10 * robot.x + 5, 10 * robot.y + 5))
+
 finished = False
+
 def crash():
-    global navx, navy,player_x, player_y, finished
-    if navx == player_x and navy == player_y:
+    global finished
+    if robot.x == player_x and robot.y == player_y:
         finished = True
     else:
         finished = False
+
 def check_collisions():
     global finished
     crash()
     if finished == True:
         key_text = Text("You are Terminated", (320, 240), size=48)
         sleep (3.00)
+
 place_robot()
 safe_player_place()
+
 finished = False
 key_text = Text("Run for your Life!", (320, 240), size=48)
 sleep(1)
