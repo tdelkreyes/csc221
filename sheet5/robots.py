656ed7eed16398ccddada581f23bf35eca7d2fed
Travis <tdelkreyes@gmail.com>
2023-07-30 15:59:36 -0400

    challenge 15 & 16 fix

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -6,29 +6,24 @@ class Robot:
     pass
 begin_graphics()
 numbots = 10
-
 def place_robots():
-    global terminators,t1000
+    global terminators
     terminators = []
-    t1000 = Robot()
-    for i in range(numbots):
-        t1000.x = randint(0, 63)
+    while len(terminators) < numbots:
+        t1000 = Robot()
+        t1000.x = randint(0, 47)
         t1000.y = randint(0, 47)
-        t1000.form = Circle((10 * t1000.x + 5, 10 * t1000.y + 5), 5, color="red", filled=True)
-        terminators.append(t1000)
+        if not crash(t1000,terminators):
+            t1000.form = Box((10 * t1000.x + 5, 10 * t1000.y + 5), 10, 10, color="red", filled=True)
+            terminators.append(t1000)
 def safe_player_place():
     global player
     player = Player()
     safety = 0
     player.x = randint(0, 63)
     player.y = randint(0, 47)
-    while safety == 0:
-        if t1000.x == player.x and t1000.y == player.y:
-            player.x = randint(0, 63)
-            player.y = randint(0, 47)
-        else:
-            player.shape = Circle((10 * player.x + 5, 10 * player.y + 5), 5, filled=True)
-            safety += 1
+    player.shape = Circle((10 * player.x + 5, 10 * player.y + 5), 5, filled=True)
+
 def move_player(): 
     global player
     key = update_when('key_pressed')
@@ -79,15 +74,16 @@ def move_robot():
         t1000.y -= 1
     move_to(t1000.form, (10 * t1000.x + 5, 10 * t1000.y + 5))
 finished = False
-def crash():
-    global finished
-    if t1000.x == player.x and t1000.y == player.y:
-        finished = True
-    else:
-        finished = False
+def crash(player,terminators):
+    for t1000 in terminators:
+        if t1000.x == player.x and t1000.y == player.y:
+            return True
+        else:
+            return False
+
 def check_collisions():
     global finished
-    crash()
+    crash(player, terminators)
     if finished == True:
         key_text = Text("You are Terminated", (320, 240), size=48, color="red")
         sleep (3.00)
@@ -97,8 +93,10 @@ finished = False
 key_text = Text("Run for your Life!", (320, 240), size=48)
 sleep(1)
 remove_from_screen(key_text)
+
 while not finished:
     move_player()
-    move_robot()
+    #move_robot()
     check_collisions()
-end_graphics()      
+
+end_graphics()         
